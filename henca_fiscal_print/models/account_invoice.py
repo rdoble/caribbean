3dcb3aeefa140d3ff7f78e511e88a5ab9b47f5f4
Juliano Henrqiuez <juliano@consultoriahenca.com>
2020-03-09 19:37:02 -0400

    Fix Tax Amount Issue

--- henca_fiscal_print/models/account_invoice.py
+++ henca_fiscal_print/models/account_invoice.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
+class AccountPayment(models.Model):
+    _inherit = "account.payment"
+
+    payment_form = fields.Selection(
+        string='Payment Form',
+        related='journal_id.payment_form'
+    )
 
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
@@ -19,6 +26,17 @@ class AccountInvoice(models.Model):
         related='ipf_printer_id.host'
     )
 
+    ipf_type = fields.Selection(
+        string="IPF Impresora",
+        readonly=True,
+        related='ipf_printer_id.ipf_type'
+    )
+
+    ipf_print_copy_number = fields.Integer(
+        string="Numero de Copias",
+        related='ipf_printer_id.print_copy_number'
+    )
+
     def _get_fiscal_printer(self):
         ipf_printer_id = self.env['ipf.printer.config'].search([
             ('user_ids', '=', self.user_id.id)
@@ -34,6 +52,36 @@ class AccountInvoice(models.Model):
     )
 
     def ipf_fiscal_print(self):
-        print('IPF FISCAL PRINT IS FIRING!')
+        pass
 
+class AccountInvoiceLine(models.Model):
+    _inherit = "account.invoice.line"
+
+    tax_amount_type = fields.Char(
+        string='Tax Computation',
+        compute='_get_tax_amount_and_type',
+        readonly=True
+    )
+
+    tax_amount = fields.Float(
+        string='Tax Amount',
+        compute='_get_tax_amount_and_type',
+        readonly=True
+    )
+
+    @api.onchange(
+        'invoice_line_tax_ids',
+        'product_id',
+        'price_unit'
+    )
+    def _get_tax_amount_and_type(self):
+        for record in self:
+            if record.invoice_line_tax_ids and record.price_unit:
+                tax = record.invoice_line_tax_ids[0]
+                tax_amount_int = int(tax.amount)
+                tax_amount = tax_amount_int if tax_amount_int in [18, 13, 11, 8, 5, 0] else 0
+                record.update({
+                    'tax_amount_type': tax.amount_type,
+                    'tax_amount': tax_amount
+                })
     
