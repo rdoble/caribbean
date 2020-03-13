4ce68d82384f590c11b9ef0f6c618a8d94c12071
Juliano Henrqiuez <juliano@consultoriahenca.com>
2020-03-13 06:24:14 -0400

    Payment Widget

--- henca_fiscal_print/models/account_invoice.py
+++ henca_fiscal_print/models/account_invoice.py
@@ -51,6 +51,21 @@ class AccountInvoice(models.Model):
         readonly=True
     )
 
+    @api.model
+    def _get_payments_vals(self):
+        payment_vals = super(AccountInvoice, self)._get_payments_vals()
+        for payment in payment_vals:
+            if payment['account_payment_id']:
+                account_payment = self.env['account.payment'].browse(payment['account_payment_id'])
+                payment_form = account_payment.journal_id.payment_form 
+                payment_description = account_payment.journal_id.display_name
+            elif payment['invoice_id']:
+                payment_description = payment['ref']
+                payment_form = 'credit_note' if 'B04' in payment_description else 'other'
+            payment['ipf_payment_form'] = payment_form
+            payment['ipf_payment_description'] = payment_description
+        return payment_vals
+
     def ipf_fiscal_print(self):
         pass
 
