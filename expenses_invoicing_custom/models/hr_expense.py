87decf1999fc021aca9991f65d29560945218554
Anthony Martinez <anthonyanselmo_martinez@bhdleon.com.do>
2020-07-02 11:15:38 -0400

    Ajustes para asignar el diario por defecto en el gasto

--- expenses_invoicing_custom/models/hr_expense.py
+++ expenses_invoicing_custom/models/hr_expense.py
@@ -1,18 +1,58 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+import logging
+_logger = logging.getLogger(__name__)
+
+try:
+    from stdnum.do import ncf as ncf_validation
+except (ImportError, IOError) as err:
+    _logger.debug(err)
 
 
 class HrExpense(models.Model):
     _inherit = 'hr.expense'
 
     supplier_id = fields.Many2one('res.partner', string='Proveedor')
+    invoice_ref = fields.Char('Referencia Factura')
+    requires_ncf = fields.Boolean('Requiere NCF', default=False)
+
+    @api.onchange('reference')
+    def _onchange_reference_validation(self):
+        NCF = self.reference if self.reference else None
+        if NCF:
+            if len(NCF) != 11:
+                raise ValidationError("El NCF debe tener 11 caracteres.")
+
+            elif NCF[-10:-8] == '02' or NCF[1:3] == '32':
+                raise ValidationError(
+                    "NCF *{}* NO corresponde con el tipo de documento\n\n"
+                    "No puede registrar Comprobantes Consumidor Final (02)"
+                    .format(NCF))
+            elif not ncf_validation.is_valid(NCF):
+                raise ValidationError(
+                    "NCF mal digitado\n\n"
+                    "El comprobante *{}* no tiene la estructura correcta "
+                    "valide si lo ha digitado correctamente".format(NCF))
+
+    @api.multi
+    def action_submit_expenses(self):
+        res = super(HrExpense, self).action_submit_expenses()
+        res['context']['default_requires_ncf'] = self.requires_ncf
+
+        if not self.requires_ncf:
+            journal_id = self.env['account.journal'].search([('purchase_type', '=', 'informal')], limit=1)
+            if journal_id:
+                res['context']['default_journal_id'] = journal_id.id
+        return res
 
 
 class HrExpenseSheet(models.Model):
     _inherit = 'hr.expense.sheet'
 
     invoice_ids = fields.One2many('account.invoice', 'expense_sheet_id', string='Facturas')
+    requires_ncf = fields.Boolean('Requiere NCF', default=False)
 
     @api.multi
     def action_expense_invoice_create(self):
@@ -29,6 +69,7 @@ class HrExpenseSheet(models.Model):
                     'type': 'in_invoice',
                     'journal_id': self.journal_id.id,
                     'reference': expense.reference,
+                    'name': expense.invoice_ref,
                     'date_invoice': expense.date,
                     'expense_sheet_id': self.id,
                     'currency_id': self.currency_id.id,
